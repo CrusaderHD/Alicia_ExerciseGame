e421cc0f96058ac96b1ea2febdd32aff0d7f941e
Hunter Derrick <CrusaderHDGaming@gmail.com>
2021-02-12 22:08:32 -0700

    Added Main Menu Functionality and fixed sprites
    Added the ability for players to navigate a semi-working main menu and to play the game. Adjusted the sprites in the first level so they are distributed in a way that makes a little more sense. Also added in the Wizard and Chest sprite.

---
+++ Alicia_ExerciseGame/Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        //Starts the game at LevelOne
+        Debug.Log("Starting Game");
+        StartCoroutine(StartGameCountdownTimer());
+
+    }
+
+    public void QuitGame()
+    {
+        //Quit Game from Main Menu
+        Debug.Log("Quitting Game from Main Menu.");
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        //Returns player to MainMenu from ingame. 
+        Debug.Log("Returning to main menu.");
+        SceneManager.LoadScene("_MainMenu", LoadSceneMode.Additive);
+    }
+
+
+    IEnumerator StartGameCountdownTimer()
+    {
+        yield return new WaitForSeconds(10); 
+        SceneManager.LoadScene("Testing");
+    }
+
+
+}
